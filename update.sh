c86c6a360e6895cfd28815ef24975169d93eb18e
Tyler Tidman <tyler.tidman@draak.ca>
2016-09-12 17:49:51 -0400

    Add legal-sized calendars to the mix

--- update.sh
+++ update.sh
@@ -2,8 +2,11 @@
 
 YEAR=$(date +%Y)
 
-make GEN_LANG=en
-make GEN_LANG=fr
+make GEN_LANG=en MEDIA=legal
+make GEN_LANG=fr MEDIA=legal
+
+make GEN_LANG=en MEDIA=letter
+make GEN_LANG=fr MEDIA=letter
 
 if [ -f build/junior_en.pdf ]; then
   mv build/junior_en.pdf junior_en.pdf
@@ -11,6 +14,14 @@ fi
 
 if [ -f build/junior_fr.pdf ]; then
   mv build/junior_fr.pdf junior_fr.pdf
+fi
+
+if [ -f build/legal_${YEAR}_en.pdf ]; then
+  mv build/legal_${YEAR}_en.pdf legal_en.pdf
+fi
+
+if [ -f build/legal_${YEAR}_fr.pdf ]; then
+  mv build/legal_${YEAR}_fr.pdf legal_fr.pdf
 fi
 
 if [ -f build/letter_${YEAR}_en.pdf ]; then
